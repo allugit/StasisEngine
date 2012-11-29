0c21eb9e974650d0e4c068f62a02d7bbc3af534e
Graeme Collins <graeme.collins.0@gmail.com>
2012-11-29 02:27:10 -0600

    Added some empty classes

---
+++ StasisEditor/StasisEditor/Main.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace StasisEditor
+{
+    public class Main : Game
+    {
+        GraphicsDeviceManager graphics;
+        public static SpriteBatch spriteBatch;
+        public static GraphicsDevice graphicsDevice;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        // Initialize
+        protected override void Initialize()
+        {
+            // XNA
+            IsMouseVisible = true;
+            graphics.PreferMultiSampling = true;
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.ApplyChanges();
+
+            // Base initialize -- calls LoadContent
+            base.Initialize();
+        }
+
+        // LoadContent
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            graphicsDevice = GraphicsDevice;
+        }
+
+        // UnloadContent
+        protected override void UnloadContent()
+        {
+        }
+
+        // Update
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        // Draw
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            base.Draw(gameTime);
+        }
+    }
+}
