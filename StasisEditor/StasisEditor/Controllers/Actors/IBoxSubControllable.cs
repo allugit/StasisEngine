aa43875b36415e6c6f5a64522eed71cc741bc3be
Graeme Collins <graeme.collins.0@gmail.com>
2012-12-08 11:46:39 -0600

    Implemented hit testing for BoxSubController.

--- StasisEditor/StasisEditor/Controllers/Actors/IBoxSubControllable.cs
+++ StasisEditor/StasisEditor/Controllers/Actors/IBoxSubControllable.cs
@@ -6,9 +6,12 @@ namespace StasisEditor.Controllers.Actors
 {
     public interface IBoxSubControllable : ISubControllable
     {
+        Vector2 getPosition();
         float getAngle();
         float getHalfWidth();
         float getHalfHeight();
+
+        void setPosition(Vector2 position);
         void setAngle(float angle);
         void setHalfWidth(float halfWidth);
         void setHalfHeight(float halfHeight);
--- StasisEditor/StasisEditor/Controllers/Actors/Input/BoxSubController.cs
+++ StasisEditor/StasisEditor/Controllers/Actors/Input/BoxSubController.cs
@@ -19,7 +19,29 @@ namespace StasisEditor.Controllers.Actors
         // hitTest
         public override bool hitTest(Vector2 worldMouse)
         {
-            return false;
+            // Get the mouse position relative to the box's center
+            Vector2 relative = _actorResourceController.getPosition() - worldMouse;
+
+            // Rotate the relative mouse position by the negative angle of the box
+            Vector2 alignedRelative = Vector2.Transform(relative, Matrix.CreateRotationZ(-_actorResourceController.getAngle()));
+
+            // Get the local, cartiasian-aligned bounding-box
+            Vector2 topLeft = -(new Vector2(_actorResourceController.getHalfWidth(), _actorResourceController.getHalfHeight()));
+            Vector2 bottomRight = -topLeft;
+
+            // Check if the relative mouse point is inside the bounding box
+            Vector2 d1, d2;
+            d1 = alignedRelative - topLeft;
+            d2 = bottomRight - alignedRelative;
+
+            // One of these components will be less than zero if the alignedRelative position is outside of the bounds
+            if (d1.X < 0 || d1.Y < 0)
+                return false;
+
+            if (d2.X < 0 || d2.Y < 0)
+                return false;
+
+            return true;
         }
 
         // handleMouseMove
