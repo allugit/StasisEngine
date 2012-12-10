de2a04e524ed8c84887cceb2acf7296a6192bffc
Graeme Collins <graeme.collins.0@gmail.com>
2012-12-10 03:44:35 -0600

    Added item resource, a few item properties, and the bare bones of a ropegun resource.

---
+++ StasisCore/Models/Resources/Items/Properties/ItemProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StasisCore.Models
+{
+    abstract public class ItemProperties
+    {
+        // clone
+        abstract public ItemProperties clone();
+    }
+
+    
+}
