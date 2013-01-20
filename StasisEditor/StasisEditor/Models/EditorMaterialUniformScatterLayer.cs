829d4fc8b0a850b62ef854527d50e9cdd4743dbb
Graeme Collins <graeme.collins.0@gmail.com>
2013-01-20 11:20:52 -0600

    Removed unnecessary files for converting between Color types.

--- StasisEditor/StasisEditor/Models/EditorMaterialUniformScatterLayer.cs
+++ StasisEditor/StasisEditor/Models/EditorMaterialUniformScatterLayer.cs
@@ -10,31 +10,16 @@ namespace StasisEditor.Models
 {
     public class EditorMaterialUniformScatterLayer : MaterialUniformScatterLayer
     {
-        private StasisEditor.Views.Controls.XNAColor _testColor;
-
         [Editor(@"System.Windows.Forms.Design.StringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
             typeof(UITypeEditor))]
         public override List<string> textureUIDs { get { return base.textureUIDs; } }
 
-        [Browsable(false)]
-        public override Color baseColor
-        {
-            get
-            {
-                return base.baseColor;
-            }
-            set
-            {
-                base.baseColor = value;
-            }
-        }
-        [EditorAttribute(typeof(StasisEditor.Views.Controls.XNAColorEditor), typeof(UITypeEditor))]
-        public StasisEditor.Views.Controls.XNAColor testColor { get { return _testColor; } set { _testColor = value; } } 
+        [EditorAttribute(typeof(XNAColorEditor), typeof(UITypeEditor))]
+        public override Color baseColor { get { return base.baseColor; } set { base.baseColor = value; } } 
 
         public EditorMaterialUniformScatterLayer(float horizontalSpacing = 32f, float verticalSpacing = 32f, float jitter = 8f)
             : base(horizontalSpacing, verticalSpacing, jitter)
         {
-            _testColor = new Views.Controls.XNAColor();
         }
 
         public EditorMaterialUniformScatterLayer(XElement data)
--- StasisEditor/StasisEditor/XNAColorEditor.cs
+++ StasisEditor/StasisEditor/XNAColorEditor.cs
@@ -5,8 +5,10 @@ using System.Drawing.Design;
 using System.Windows.Forms;
 using System.Windows.Forms.Design;
 
-namespace StasisEditor.Views.Controls
+namespace StasisEditor
 {
+    using XNAColor = Microsoft.Xna.Framework.Color;
+
     public class XNAColorEditor : UITypeEditor
     {
         private IWindowsFormsEditorService service;
@@ -25,9 +27,9 @@ namespace StasisEditor.Views.Controls
             {
                 XNAColor color = (XNAColor)value;
                 ColorDialog selectionControl = new ColorDialog();
-                selectionControl.Color = Color.FromArgb(color.GetARGB());
+                selectionControl.Color = Color.FromArgb(BitConverter.ToInt32(new byte[] { color.B, color.G, color.R, color.A }, 0));
                 selectionControl.ShowDialog();
-                value = new XNAColor(selectionControl.Color.ToArgb());
+                value = new XNAColor((int)selectionControl.Color.R, (int)selectionControl.Color.G, (int)selectionControl.Color.B);
             }
 
             return value;
