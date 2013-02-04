5cc3cf5999f85128f60c092d7f8bfd1d9ae65f11
Graeme Collins <graeme.collins.0@gmail.com>
2013-02-04 02:49:50 -0600

    Created an actor component interface that will be used to identify and act on controls during hit testing.

---
+++ StasisEditor/StasisEditor/Models/IActorComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StasisEditor.Models
+{
+    public enum ActorComponentType
+    {
+        Point,
+        Box,
+        Circle,
+        LineSegment
+    };
+
+    public interface IActorComponent
+    {
+        ActorComponentType componentType { get; }
+    }
+}
