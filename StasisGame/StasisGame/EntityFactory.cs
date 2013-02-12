f34a8eea7ad657e08887436ae6b9dbffe6a60d29
Graeme Collins <graeme.collins.0@gmail.com>
2013-02-11 19:10:10 -0600

    Unfinished changes.

--- StasisGame/StasisGame/EntityFactory.cs
+++ StasisGame/StasisGame/EntityFactory.cs
@@ -4,6 +4,9 @@ using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Box2D.XNA;
+using StasisCore;
+using StasisCore.Models;
+using StasisCore.Controllers;
 using StasisGame.Systems;
 using StasisGame.Components;
 using StasisGame.Managers;
@@ -21,12 +24,22 @@ namespace StasisGame
             _entityManager = entityManager;
         }
 
+        private BodyRenderComponent createBodyRenderComponent(XElement data, PhysicsComponent physicsComponent)
+        {
+            RenderSystem renderSystem = (RenderSystem)_systemManager.getSystem(SystemType.Render);
+            Material material = new Material(ResourceController.getResource(data.Attribute("material_uid").Value));
+            Texture texture = renderSystem.materialRenderer.renderMaterial(material, polygonPoints, 1f);
+
+            BodyRenderComponent renderComponent = new BodyRenderComponent(texture, vertices, worldMatrix, primitiveCount, layerDepth);
+        }
+
         public void createBox(XElement data)
         {
             World world = (_systemManager.getSystem(SystemType.Physics) as PhysicsSystem).world;
             int entityId = _entityManager.createEntity();
-            _entityManager.addComponent(entityId, new PhysicsComponent(world, data));
-            _entityManager.addComponent(entityId, new BodyRenderComponent(
+            PhysicsComponent physicsComponent = new PhysicsComponent(world, data);
+            _entityManager.addComponent(entityId, physicsComponent);
+            _entityManager.addComponent(entityId, createBodyRenderComponent(data, physicsComponent));
         }
 
         public void createCircle(XElement data)
