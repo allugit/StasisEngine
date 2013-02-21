a7c07cc8a4e1f6be57cb8f210176ca1b406eb5fc
Graeme Collins <graeme.collins.0@gmail.com>
2013-02-21 09:56:05 -0600

    Added handler/subscription structure.

---
+++ StasisCore/IEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StasisCore
+{
+    public interface IEventHandler
+    {
+        void trigger(GameEvent e);
+    }
+}
--- StasisGame/StasisGame/Systems/EventSystem.cs
+++ StasisGame/StasisGame/Systems/EventSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using StasisCore;
 using StasisGame.Managers;
 
 namespace StasisGame.Systems
@@ -8,6 +9,8 @@ namespace StasisGame.Systems
     {
         private SystemManager _systemManager;
         private EntityManager _entityManager;
+        private Dictionary<GameEventType, Dictionary<int, List<IEventHandler>>> _handlers;
+        private Dictionary<GameEventType, Dictionary<int, List<int>>> _subscriptions;
 
         public int defaultPriority { get { return 50; } }
         public SystemType systemType { get { return SystemType.Event; } }
@@ -16,6 +19,28 @@ namespace StasisGame.Systems
         {
             _systemManager = systemManager;
             _entityManager = entityManager;
+            _handlers = new Dictionary<GameEventType, Dictionary<int, List<IEventHandler>>>();
+            _subscriptions = new Dictionary<GameEventType, Dictionary<int, List<int>>>();
+        }
+
+        public void addHandler(GameEventType gameEventType, int handlerEntityId, IEventHandler handler)
+        {
+            if (!_handlers.ContainsKey(gameEventType))
+                _handlers.Add(gameEventType, new Dictionary<int, List<IEventHandler>>());
+            if (!_handlers[gameEventType].ContainsKey(handlerEntityId))
+                _handlers[gameEventType].Add(handlerEntityId, new List<IEventHandler>());
+            if (!_handlers[gameEventType][handlerEntityId].Contains(handler))
+                _handlers[gameEventType][handlerEntityId].Add(handler);
+        }
+
+        public void subscribe(GameEventType gameEventType, int handlerEntityId, int senderEntityId)
+        {
+            if (!_subscriptions.ContainsKey(gameEventType))
+                _subscriptions.Add(gameEventType, new Dictionary<int, List<int>>());
+            if (!_subscriptions[gameEventType].ContainsKey(handlerEntityId))
+                _subscriptions[gameEventType].Add(handlerEntityId, new List<int>());
+            if (!_subscriptions[gameEventType][handlerEntityId].Contains(senderEntityId))
+                _subscriptions[gameEventType][handlerEntityId].Add(senderEntityId);
         }
 
         public void update()
