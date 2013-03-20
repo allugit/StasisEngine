e40d85b3cd2e03dedc85b74011cae9ce9ad2ae27
Graeme Collins <graeme.collins.0@gmail.com>
2013-03-19 23:44:40 -0500

    Moved activation of item into equipment system.

--- StasisGame/StasisGame/Systems/EquipmentSystem.cs
+++ StasisGame/StasisGame/Systems/EquipmentSystem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using StasisGame.Components;
 using StasisGame.Managers;
 
@@ -31,6 +33,26 @@ namespace StasisGame.Systems
 
         public void update()
         {
+            PlayerSystem playerSystem = _systemManager.getSystem(SystemType.Player) as PlayerSystem;
+
+            if (playerSystem != null)
+            {
+                int playerId = playerSystem.playerId;
+                ToolbarComponent playerToolbar = _entityManager.getComponent(playerId, ComponentType.Toolbar) as ToolbarComponent;
+                ItemComponent selectedItem = playerToolbar.selectedItem;
+
+                if (selectedItem != null)
+                {
+                    bool mouseLeftDown = InputSystem.newMouseState.LeftButton == ButtonState.Pressed && InputSystem.oldMouseState.LeftButton == ButtonState.Released;
+                    bool leftTriggerDown = InputSystem.newGamepadState.IsConnected && InputSystem.newGamepadState.Triggers.Left > 0.5f && InputSystem.oldGamepadState.Triggers.Left <= 0.5f;
+
+                    if (mouseLeftDown || leftTriggerDown)
+                    {
+                        Console.WriteLine("Firing: {0}", selectedItem.itemType);
+                    }
+                }
+            }
+
         }
     }
 }
--- StasisGame/StasisGame/Systems/InputSystem.cs
+++ StasisGame/StasisGame/Systems/InputSystem.cs
@@ -12,12 +12,12 @@ namespace StasisGame.Systems
         private SystemManager _systemManager;
         private EntityManager _entityManager;
 
-        private KeyboardState _newKeyState;
-        private KeyboardState _oldKeyState;
-        private MouseState _newMouseState;
-        private MouseState _oldMouseState;
-        private GamePadState _newGamepadState;
-        private GamePadState _oldGamepadState;
+        public static KeyboardState newKeyState;
+        public static KeyboardState oldKeyState;
+        public static MouseState newMouseState;
+        public static MouseState oldMouseState;
+        public static GamePadState newGamepadState;
+        public static GamePadState oldGamepadState;
 
         public int defaultPriority { get { return 1; } }
         public SystemType systemType { get { return SystemType.Input; } }
@@ -32,22 +32,22 @@ namespace StasisGame.Systems
         {
             List<InputComponent> inputComponents = _entityManager.getComponents<InputComponent>(ComponentType.Input);
 
-            _oldKeyState = _newKeyState;
-            _oldMouseState = _newMouseState;
-            _oldGamepadState = _newGamepadState;
+            oldKeyState = newKeyState;
+            oldMouseState = newMouseState;
+            oldGamepadState = newGamepadState;
 
-            _newKeyState = Keyboard.GetState();
-            _newMouseState = Mouse.GetState();
-            _newGamepadState = GamePad.GetState(PlayerIndex.One);
+            newKeyState = Keyboard.GetState();
+            newMouseState = Mouse.GetState();
+            newGamepadState = GamePad.GetState(PlayerIndex.One);
 
             for (int i = 0; i < inputComponents.Count; i++)
             {
-                inputComponents[i].newKeyState = _newKeyState;
-                inputComponents[i].newMouseState = _newMouseState;
-                inputComponents[i].newGamepadState = _newGamepadState;
-                inputComponents[i].oldKeyState = _oldKeyState;
-                inputComponents[i].oldMouseState = _oldMouseState;
-                inputComponents[i].oldGamepadState = _oldGamepadState;
+                inputComponents[i].newKeyState = newKeyState;
+                inputComponents[i].newMouseState = newMouseState;
+                inputComponents[i].newGamepadState = newGamepadState;
+                inputComponents[i].oldKeyState = oldKeyState;
+                inputComponents[i].oldMouseState = oldMouseState;
+                inputComponents[i].oldGamepadState = oldGamepadState;
             }
         }
     }
