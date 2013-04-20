bb25ad07446587bc49ccef313eaaf600a2b4fb43
Graeme Collins <graeme.collins.0@gmail.com>
2013-04-20 02:16:59 -0500

    Updated WorldMapScreen with new texture, and added boundary checks (Needs work, scaling isn't taken into account when determining boundaries).

--- StasisGame/StasisGame/UI/WorldMapScreen.cs
+++ StasisGame/StasisGame/UI/WorldMapScreen.cs
@@ -10,48 +10,29 @@ namespace StasisGame.UI
 {
     public class WorldMapScreen : Screen
     {
-        private struct PositionTexture
-        {
-            public Vector2 position;
-            public Texture2D texture;
-            public PositionTexture(Vector2 position, Texture2D texture)
-            {
-                this.position = position;
-                this.texture = texture;
-            }
-        };
-
         private LoderGame _game;
         private SpriteBatch _spriteBatch;
-        private ContentManager _content;
-        private List<PositionTexture> _textures;
-        private int _worldWidth = 8400;
-        private int _worldHeight = 5250;
-        private Matrix _view;
+        private Texture2D _texture;
         private float _scale;
         private Vector2 _currentScreenCenter;
         private Vector2 _targetScreenCenter;
+        private Vector2 _topLeft;
+        private Vector2 _bottomRight;
+        private Vector2 _halfTextureSize;
+        private Texture2D _pixel;
 
         public WorldMapScreen(LoderGame game) : base(ScreenType.WorldMap)
         {
             _game = game;
             _spriteBatch = _game.spriteBatch;
-            _textures = new List<PositionTexture>();
-            _content = new ContentManager(_game.Services);
-            _content.RootDirectory = "Content";
             _scale = 1f;
-            int widthSegments = (int)Math.Ceiling((float)_worldWidth / 2048f);
-            int heightSegments = (int)Math.Ceiling((float)_worldHeight / 2048f);
 
-            int count = 0;
-            for (int j = 0; j < heightSegments; j++)
-            {
-                for (int i = 0; i < widthSegments; i++)
-                {
-                    _textures.Add(new PositionTexture(new Vector2(2048f * i, 2048f * j), ResourceManager.getTexture("world_map_" + count.ToString())));
-                    count++;
-                }
-            }
+            _texture = ResourceManager.getTexture("world_map");
+            _pixel = new Texture2D(_spriteBatch.GraphicsDevice, 1, 1);
+            _pixel.SetData<Color>(new[] { Color.White });
+            _halfTextureSize = new Vector2(_texture.Width, _texture.Height) / 2f;
+            _topLeft = Vector2.Zero;
+            _bottomRight = new Vector2(_texture.Width, _texture.Height) - new Vector2(_spriteBatch.GraphicsDevice.Viewport.Width, _spriteBatch.GraphicsDevice.Viewport.Height);
         }
 
         public override void update()
@@ -66,30 +47,32 @@ namespace StasisGame.UI
 
             if (_newGamepadState.IsConnected)
             {
-                _targetScreenCenter += _newGamepadState.ThumbSticks.Left * 7 * new Vector2(-1, 1);
-                _targetScreenCenter += _newGamepadState.ThumbSticks.Right * 7 * new Vector2(-1, 1);
-                _currentScreenCenter += (_targetScreenCenter - _currentScreenCenter) / 11f;
+                _targetScreenCenter += _newGamepadState.ThumbSticks.Left * 7 * new Vector2(1, -1);
+                _targetScreenCenter += _newGamepadState.ThumbSticks.Right * 7 * new Vector2(1, -1);
 
                 _scale = Math.Max(0.5f, _scale - _newGamepadState.Triggers.Left / 500f);
                 _scale = Math.Min(1f, _scale + _newGamepadState.Triggers.Right / 500f);
             }
+
+            if (_newKeyState.IsKeyDown(Keys.Left) || _newKeyState.IsKeyDown(Keys.A))
+                _targetScreenCenter += new Vector2(7, 0);
+            if (_newKeyState.IsKeyDown(Keys.Right) || _newKeyState.IsKeyDown(Keys.D))
+                _targetScreenCenter += new Vector2(-7, 0);
+            if (_newKeyState.IsKeyDown(Keys.Up) || _newKeyState.IsKeyDown(Keys.W))
+                _targetScreenCenter += new Vector2(0, 7);
+            if (_newKeyState.IsKeyDown(Keys.Down) || _newKeyState.IsKeyDown(Keys.S))
+                _targetScreenCenter += new Vector2(0, -7);
+
+            _targetScreenCenter = Vector2.Max(_topLeft, Vector2.Min(_bottomRight, _targetScreenCenter));
+            _currentScreenCenter += (_targetScreenCenter - _currentScreenCenter) / 11f;
 
             base.update();
         }
 
         public override void draw()
         {
-            _view =
-                Matrix.CreateTranslation(new Vector3(-_worldWidth, -_worldHeight, 0) / 2f) *
-                Matrix.CreateTranslation(new Vector3(_currentScreenCenter, 0)) *
-                Matrix.CreateScale(_scale) *
-                Matrix.CreateTranslation(new Vector3(_game.GraphicsDevice.Viewport.Width, _game.GraphicsDevice.Viewport.Height, 0) / 2f);
-
-            for (int i = 0; i < _textures.Count; i++)
-            {
-                Vector2 position = Vector2.Transform(_textures[i].position, _view);
-                _spriteBatch.Draw(_textures[i].texture, position, _textures[i].texture.Bounds, Color.White, 0f, Vector2.Zero, _scale, SpriteEffects.None, 0f);
-            }
+            _spriteBatch.Draw(_texture, -_currentScreenCenter, _texture.Bounds, Color.White, 0f, Vector2.Zero, _scale, SpriteEffects.None, 0f);
+            _spriteBatch.Draw(_pixel, Vector2.Zero, new Rectangle(0, 0, 25, 25), Color.Red);
 
             base.draw();
         }
