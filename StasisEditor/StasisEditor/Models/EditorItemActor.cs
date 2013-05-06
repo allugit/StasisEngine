60d561a3e38d54409abd574c881d83bec4b3036f
Graeme Collins <graeme.collins.0@gmail.com>
2013-05-06 02:44:49 -0500

    Fixed moving item actor in editor while dragging the screen.

--- StasisEditor/StasisEditor/Models/EditorItemActor.cs
+++ StasisEditor/StasisEditor/Models/EditorItemActor.cs
@@ -100,7 +100,8 @@ namespace StasisEditor.Models
 
             if (selected)
             {
-                _position += worldDelta;
+                if (!_level.controller.ctrl)
+                    _position += worldDelta;
 
                 if (_level.controller.isKeyPressed(Keys.Escape))
                     deselect();
