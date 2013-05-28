cf805f3168dae5e2cbfb7b21dc866018ef99f10d
Graeme Collins <graeme.collins.0@gmail.com>
2013-05-28 09:01:11 -0500

    Removed '#if WINDOWS || XBOX' in Program.cs

--- StasisGame/StasisGame/Program.cs
+++ StasisGame/StasisGame/Program.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace StasisGame
 {
-#if WINDOWS || XBOX
     static class Program
     {
         /// <summary>
@@ -16,6 +15,5 @@ namespace StasisGame
             }
         }
     }
-#endif
 }
 
