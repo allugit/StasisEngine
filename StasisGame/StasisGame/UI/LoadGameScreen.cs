644aff5de7acba0dc584ec79195809b36e92c547
Graeme Collins <graeme.collins.0@gmail.com>
2013-07-10 03:35:23 -0500

    Reimplemented load game screen.

--- StasisGame/StasisGame/UI/LoadGameScreen.cs
+++ StasisGame/StasisGame/UI/LoadGameScreen.cs
@@ -15,7 +15,11 @@ namespace StasisGame.UI
         private LoderGame _game;
         private Texture2D _logo;
         private ContentManager _content;
+        private SpriteFont _savedGameFont;
+        private BluePane _container;
+        private List<StonePane> _bars;
         private List<TextButton> _savedGameButtons;
+        private TextButton _selectedButton;
 
         public LoadGameScreen(LoderGame game)
             : base(game.spriteBatch, ScreenType.LoadGameMenu)
@@ -24,9 +28,52 @@ namespace StasisGame.UI
             _content = new ContentManager(game.Services);
             _content.RootDirectory = "Content";
             _logo = _content.Load<Texture2D>("logo");
+            _savedGameFont = _content.Load<SpriteFont>("load_game_menu/saved_game_font");
             _savedGameButtons = new List<TextButton>();
+            _bars = new List<StonePane>();
 
-            createUIComponents();
+            List<XElement> playerSaves = DataManager.loadPlayerSaves();
+            Vector2 initialPosition = new Vector2(-230, -180);
+
+            _container = new BluePane(
+                _spriteBatch,
+                UIAlignment.MiddleCenter,
+                -250,
+                -200,
+                500,
+                400);
+
+            foreach (XElement playerSave in playerSaves)
+            {
+                int slot = int.Parse(playerSave.Attribute("slot").Value);
+                string text = slot.ToString() + " - " + playerSave.Attribute("name").Value;
+
+                _bars.Add(new StonePane(
+                    _spriteBatch,
+                    UIAlignment.MiddleCenter,
+                    (int)initialPosition.X - 10,
+                    (int)(initialPosition.Y) + _bars.Count * 48 - 10,
+                    480,
+                    40));
+
+                _savedGameButtons.Add(new TextButton(
+                    _game.spriteBatch,
+                    _savedGameFont,
+                    UIAlignment.MiddleCenter,
+                    (int)initialPosition.X,
+                    (int)(initialPosition.Y) + _savedGameButtons.Count * 48,
+                    4,
+                    TextAlignment.Left,
+                    text,
+                    1,
+                    Color.White,
+                    new Color(0.7f, 0.7f, 0.7f),
+                    () =>
+                    {
+                        _game.closeLoadGameMenu();
+                        _game.loadGame(slot);
+                    }));
+            }
         }
 
         ~LoadGameScreen()
@@ -34,40 +81,42 @@ namespace StasisGame.UI
             _content.Unload();
         }
 
-        private void createUIComponents()
-        {
-            List<XElement> playerSaves = DataManager.loadPlayerSaves();
-            Vector2 initialPosition = new Vector2(-200, 300);
-
-            /*
-            foreach (XElement playerSave in playerSaves)
-            {
-                int slot = int.Parse(playerSave.Attribute("slot").Value);
-                string text = slot.ToString() + " - " + playerSave.Attribute("name").Value;
-                TextButton button = new TextButton(
-                    _game.spriteBatch,
-                    _arial,
-                    Color.White,
-                    (int)initialPosition.X,
-                    (int)(initialPosition.Y) + _saveButtons.Count * 24,
-                    text,
-                    UIAlignment.TopCenter,
-                    () =>
-                    {
-                        _game.closeLoadGameMenu();
-                        _game.loadGame(slot);
-                    });
-                _saveButtons.Add(button);
-            }*/
-        }
-
         public override void update()
         {
+            bool mouseOverTextButton = false;
+            Vector2 mouse = new Vector2(_newMouseState.X, _newMouseState.Y);
+
+            // Update input
             base.update();
 
             // Handle button mouse input
-            for (int i = 0; i < _savedGameButtons.Count; i++)
+            foreach (TextButton button in _savedGameButtons)
             {
+                if (button.hitTest(mouse))
+                {
+                    if (_selectedButton != button)
+                    {
+                        if (_selectedButton != null)
+                            _selectedButton.mouseOut();
+
+                        button.mouseOver();
+                    }
+
+                    mouseOverTextButton = true;
+                    _selectedButton = button;
+
+                    if (_newMouseState.LeftButton == ButtonState.Pressed && _oldMouseState.LeftButton == ButtonState.Released)
+                        button.activate();
+
+                    break;
+                }
+            }
+            if (!mouseOverTextButton)
+            {
+                if (_selectedButton != null)
+                    _selectedButton.mouseOut();
+
+                _selectedButton = null;
             }
 
             // Background renderer
@@ -83,6 +132,13 @@ namespace StasisGame.UI
 
             // Draw logo
             _spriteBatch.Draw(_logo, new Vector2((int)(_game.GraphicsDevice.Viewport.Width - _logo.Width), 0), _logo.Bounds, Color.White, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+
+            // Draw container
+            _container.draw();
+
+            // Draw bars
+            foreach (StonePane bar in _bars)
+                bar.draw();
 
             // Draw buttons
             for (int i = 0; i < _savedGameButtons.Count; i++)
