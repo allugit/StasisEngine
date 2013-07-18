63af985289acf564da11d92cf28af4ecf09255aa
Graeme Collins <graeme.collins.0@gmail.com>
2013-07-18 02:03:03 -0500

    Changed editor's profile to HiDef.

--- StasisEditor/StasisEditor/GraphicsDeviceService.cs
+++ StasisEditor/StasisEditor/GraphicsDeviceService.cs
@@ -62,7 +62,7 @@ namespace StasisEditor
             parameters.IsFullScreen = false;
 
             graphicsDevice = new GraphicsDevice(GraphicsAdapter.DefaultAdapter,
-                                                GraphicsProfile.Reach,
+                                                GraphicsProfile.HiDef,
                                                 parameters);
         }
 
