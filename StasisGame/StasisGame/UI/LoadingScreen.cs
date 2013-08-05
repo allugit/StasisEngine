1a2e4497ab8d47d3eb043f2c4cc2eb407ade3b7e
Graeme Collins <graeme.collins.0@gmail.com>
2013-08-05 05:29:04 -0500

    Added a background to the loading screen.

--- StasisGame/StasisGame/UI/LoadingScreen.cs
+++ StasisGame/StasisGame/UI/LoadingScreen.cs
@@ -10,6 +10,7 @@ namespace StasisGame.UI
     public class LoadingScreen : Screen
     {
         private List<Texture2D> _textures;
+        private Texture2D _background;
         private ContentManager _content;
         private int _textureIndex;
 
@@ -23,6 +24,7 @@ namespace StasisGame.UI
             {
                 _textures.Add(_content.Load<Texture2D>(string.Format("loading_screen/gear_{0}", i)));
             }
+            _background = _content.Load<Texture2D>("loading_screen/background");
         }
 
         public override void update()
@@ -34,7 +36,8 @@ namespace StasisGame.UI
         {
             Texture2D texture = _textures[_textureIndex];
 
-            _spriteBatch.Draw(texture, texture.Bounds, texture.Bounds, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.1f);
+            _spriteBatch.Draw(_background, _background.Bounds, _background.Bounds, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.11f);
+            _spriteBatch.Draw(texture, new Rectangle(0, -110, texture.Width, texture.Height), texture.Bounds, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.1f);
 
             base.draw();
         }
