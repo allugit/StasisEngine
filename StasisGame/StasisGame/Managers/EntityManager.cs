795fce5b669bfde583c766809e7dd5326df776ed
Graeme Collins <graeme.collins.0@gmail.com>
2013-08-14 05:20:09 -0500

    Restructured entity manager.

--- StasisGame/StasisGame/Managers/EntityManager.cs
+++ StasisGame/StasisGame/Managers/EntityManager.cs
@@ -13,51 +13,48 @@ namespace StasisGame.Managers
     public class EntityManager
     {
         private SystemManager _systemManager;
-        private Dictionary<int, List<IComponent>> _entities;
+        private Dictionary<string, Dictionary<int, List<IComponent>>> _entities;
         private EntityFactory _factory;
-        private List<int> _reservedIds;
+        private Dictionary<string, List<int>> _reservedIds;
 
         public EntityFactory factory { get { return _factory; } }
-
-        public int newId
-        {
-            get
-            {
-                int i = 0;
-                while (_entities.ContainsKey(i) || _reservedIds.Contains(i))
-                    i++;
-                return i;
-            }
-        }
 
         public EntityManager(SystemManager systemManager)
         {
             _systemManager = systemManager;
-            _entities = new Dictionary<int, List<IComponent>>();
+            _entities = new Dictionary<string, Dictionary<int, List<IComponent>>>();
             _factory = new EntityFactory(_systemManager, this);
-            _reservedIds = new List<int>();
+            _reservedIds = new Dictionary<string, List<int>>();
+        }
+
+        public int getNewId(string levelUid)
+        {
+            int i = 0;
+            while (_entities[levelUid].ContainsKey(i) || _reservedIds[levelUid].Contains(i))
+                i++;
+            return i;
         }
 
         // Create an entity
-        public int createEntity()
+        public int createEntity(string levelUid)
         {
-            int id = newId;
-            _entities.Add(id, new List<IComponent>());
+            int id = getNewId(levelUid);
+            _entities[levelUid].Add(id, new List<IComponent>());
             return id;
         }
 
         // Create an entity with a specific id -- can use reserved ids
-        public int createEntity(int id)
+        public int createEntity(string levelUid, int id)
         {
-            _entities.Add(id, new List<IComponent>());
+            _entities[levelUid].Add(id, new List<IComponent>());
             return id;
         }
 
         // Reserve entity id
-        public void reserveEntityId(int id)
+        public void reserveEntityId(string levelUid, int id)
         {
-            Debug.Assert(!_reservedIds.Contains(id));
-            _reservedIds.Add(id);
+            Debug.Assert(!_reservedIds[levelUid].Contains(id));
+            _reservedIds[levelUid].Add(id);
         }
 
         // Clear reserved entity ids
@@ -67,21 +64,21 @@ namespace StasisGame.Managers
         }
 
         // Kill an entity
-        public void killEntity(int id)
+        public void killEntity(string levelUid, int id)
         {
-            _entities.Remove(id);
+            _entities[levelUid].Remove(id);
         }
 
         // Kill all entities
-        public void killAllEntities()
+        public void killAllEntities(string levelUid)
         {
-            killAllEntities(new List<int>());
+            killAllEntities(levelUid, new List<int>());
         }
-        public void killAllEntities(List<int> excludedIds)
+        public void killAllEntities(string levelUid, List<int> excludedIds)
         {
             List<int> entitiesToRemove = new List<int>();
 
-            foreach (KeyValuePair<int, List<IComponent>> pair in _entities)
+            foreach (KeyValuePair<int, List<IComponent>> pair in _entities[levelUid])
             {
                 if (!excludedIds.Contains(pair.Key))
                     entitiesToRemove.Add(pair.Key);
@@ -89,35 +86,35 @@ namespace StasisGame.Managers
 
             foreach (int id in entitiesToRemove)
             {
-                _entities.Remove(id);
+                _entities[levelUid].Remove(id);
             }
         }
 
         // Add a component to an entity
-        public void addComponent(int id, IComponent component)
+        public void addComponent(string levelUid, int id, IComponent component)
         {
-            _entities[id].Add(component);
+            _entities[levelUid][id].Add(component);
         }
 
         // Remove a component from an entity, based on the type of component
-        public void removeComponent(int id, ComponentType componentType)
+        public void removeComponent(string levelUid, int id, ComponentType componentType)
         {
-            IComponent component = getComponent(id, componentType);
-            removeComponent(id, component);
+            IComponent component = getComponent(levelUid, id, componentType);
+            removeComponent(levelUid, id, component);
         }
 
         // Remove a component from an entity, based on an instance of the component
-        public void removeComponent(int id, IComponent component)
+        public void removeComponent(string levelUid, int id, IComponent component)
         {
-            _entities[id].Remove(component);
+            _entities[levelUid][id].Remove(component);
         }
 
         // Get a component from an entity
-        public IComponent getComponent(int id, ComponentType componentType)
+        public IComponent getComponent(string levelUid, int id, ComponentType componentType)
         {
-            if (_entities.ContainsKey(id))
+            if (_entities[levelUid].ContainsKey(id))
             {
-                foreach (IComponent component in _entities[id])
+                foreach (IComponent component in _entities[levelUid][id])
                 {
                     if (component.componentType == componentType)
                         return component;
@@ -127,13 +124,13 @@ namespace StasisGame.Managers
         }
 
         // Tries to get a component from an entity, and returns true on success
-        public bool tryGetComponent(int id, ComponentType componentType, out IComponent component)
+        public bool tryGetComponent(string levelUid, int id, ComponentType componentType, out IComponent component)
         {
             component = null;
 
-            if (_entities.ContainsKey(id))
+            if (_entities[levelUid].ContainsKey(id))
             {
-                foreach (IComponent c in _entities[id])
+                foreach (IComponent c in _entities[levelUid][id])
                 {
                     if (c.componentType == componentType)
                     {
@@ -146,10 +143,10 @@ namespace StasisGame.Managers
         }
 
         // Get all components of a certain type
-        public List<T> getComponents<T>(ComponentType componentType) where T : IComponent
+        public List<T> getComponents<T>(string levelUid, ComponentType componentType) where T : IComponent
         {
             List<T> results = new List<T>();
-            foreach (List<IComponent> components in _entities.Values)
+            foreach (List<IComponent> components in _entities[levelUid].Values)
             {
                 for (int i = 0; i < components.Count; i++)
                 {
@@ -163,12 +160,12 @@ namespace StasisGame.Managers
         }
 
         // Get all entities posessing a type of component
-        public List<int> getEntitiesPosessing(ComponentType componentType)
+        public List<int> getEntitiesPosessing(string levelUid, ComponentType componentType)
         {
             List<int> results = new List<int>();
-            foreach (int e in _entities.Keys)
+            foreach (int e in _entities[levelUid].Keys)
             {
-                foreach (IComponent component in _entities[e])
+                foreach (IComponent component in _entities[levelUid][e])
                 {
                     if (component.componentType == componentType)
                     {
@@ -181,10 +178,10 @@ namespace StasisGame.Managers
         }
 
         // Get all of an entity's components
-        public List<IComponent> getEntityComponents(int entityId)
+        public List<IComponent> getEntityComponents(string levelUid, int entityId)
         {
-            if (_entities.ContainsKey(entityId))
-                return _entities[entityId];
+            if (_entities[levelUid].ContainsKey(entityId))
+                return _entities[levelUid][entityId];
 
             return null;
         }
